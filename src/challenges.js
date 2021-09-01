5c00df93b07ea136d5880864cfdc7d38848de701
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-01 00:03:52 -0300

    Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -1,43 +1,62 @@
 // Desafio 1
+// dois valores parametros
+// SE AMBOS forem verdadeiros = true
+// SE algum dos valores for false = false
 function compareTrue(valor1, valor2) {
-  if (valor1 && valor2 === true) {
-    return true;
-  } else {
-    return false;
-  }
+if (valor1 && valor2 === true) {
+  return true;
+} else {
+  return false;
+}
 }
 
 // Desafio 2
+// parametros são base e height
+// formula = base x altura / 2
+// retornar o cálculo
 function calcArea(base, height) {
-  return (base * height) / 2;
+return (base * height) / 2;
 }
 
 // Desafio 3
+/** https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254 */
+// parametro é string
+// transformar string em array
+// retornar array de strings 
+// o que definirá a separação das strings são os espaços
 function splitSentence(string) {
-  return string.split(" ");
+return string.split(" ");
 }
 
 // Desafio 4
-function concatName(array) {
+  /** https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/concat */
   // parametro é array
   // pegar a ultima e a primeira  posição do array 
   // transformar o array em uma string e retornar
-  let firstPosition = array[0];
-  let lastPosition = array[array.length -1];
-  let concatArrays = array[array.length - 1] + ", ".concat(array[0]);
-  let result = concatArrays;
-  return result;
+function concatName(array) {
+
+let firstPosition = array[0];
+let lastPosition = array[array.length -1];
+let concatArrays = array[array.length - 1] + ", ".concat(array[0]);
+let result = concatArrays;
+return result;
 }
 
 // Desafio 5
+// parametros wins,ties (vitória e empate)
+// retorna total de pontos
+// win = 3 ties = 1
 function footballPoints(wins, ties) {
-  wins = wins * 3;
-  ties = ties * 1;
-  let totalPoints = wins + ties;
-  return totalPoints;
+wins = wins * 3;
+ties = ties * 1;
+let totalPoints = wins + ties;
+return totalPoints;
 }
 
 // Desafio 6
+// parametro arrayNumber
+// descobrir o maior numero
+// contar  e retornar quantas vezes o maior numero aparece
 function highestCount(arrayNumber) {
   let maiorNumber = arrayNumber[0];
   let contMaiorNumber = 0;
@@ -55,9 +74,15 @@ function highestCount(arrayNumber) {
 }
 
 // Desafio 7
+// Consultei a mentoria técnica para resolver esse problema e obtive a recomendação do Caíque para a leitura da Documentação abaixo 
+/** https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/abs */
+// parametros mouse,cat1,cat2
+// calcular distância entre rato e gato 1
+// calcular distância entre rato e gato 2
+// Retornar o que estiver mais perto do numero do rato
 function catAndMouse(mouse, cat1, cat2) {
-  let distanciacat1 = mouse - cat1;
-  let distanciacat2 = mouse - cat2;
+  let distanciacat1 = Math.abs(mouse - cat1);
+  let distanciacat2 = Math.abs(mouse - cat2);
   if(distanciacat1 < distanciacat2) {
     return "cat1";
   } else if (distanciacat2 < distanciacat1) {
@@ -68,13 +93,14 @@ function catAndMouse(mouse, cat1, cat2) {
 }
 
 // Desafio 8
-function fizzBuzz(arrayNumber) {
 // parametro arrayNumber
 // retornar tudo em uníco array 
 // se divisivel por 3 e 5 adiciona "fizz buzz"
 // se divisivel por 3 adiciona "fizz"
 // se divisivel por 5 adiciona "buzz"
 // nenhum dos casos adiciona "bug!"
+function fizzBuzz(arrayNumber) {
+
 let result = [];
   for(let index = 0; index < arrayNumber.length; index += 1 ) {
   if (arrayNumber[index] % 3 === 0 && arrayNumber[index] % 5 === 0) {

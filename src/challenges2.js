f27d252df65426e8f5822317b4afdd0e78f15c7a
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-07 19:37:50 -0300

    Requisito 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -18,10 +18,44 @@ function techList(array, nick) {
   }
 }
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(arrayNumber) {
+  // parametro array com 11 numeros
+  // condições: array maior que 11 = tamanho incorreto
+  // menor que 0 ou maior que 9 e que repita mais que 3 vezes = não é possível
+  // Verifica se o tamanho do Array é maior ou menor que 11
+  if (arrayNumber.length !== 11) {
+    return "Array com tamanho incorreto.";
+  } 
+  // Verifica se cada número é maior que 0 e menor que 9
+  for (let index = 0; index < arrayNumber.length; index += 1) {
+    if (arrayNumber[index] < 0 || arrayNumber[index] > 9) {
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  }
+  // Verifica quantas vezes cada número se repete
+  for (let index = 0; index < arrayNumber.length; index += 1){
+    let number = arrayNumber[index];
+    let cont = 0;
+    for (let i = 0; i < arrayNumber.length; i += 1) {
+      if ( arrayNumber[i] === number) {
+         cont += 1
+      }
+      if ( cont >= 3) {
+       return "não é possível gerar um número de telefone com esses valores"
+      }
+    }
+  }
+// Gera o Número de telefone
+let numberGenerate = ['(', arrayNumber[0], arrayNumber[1], ')', ' '];
+for (let index = 2; index < arrayNumber.length; index += 1) {
+  if (index === 6){
+    numberGenerate.push(arrayNumber[index], '-');
+  } else {
+    numberGenerate.push(arrayNumber[index])
+  }
 }
-
+return numberGenerate.join('');
+}
 // Desafio 12
 function triangleCheck() {
   // seu código aqui

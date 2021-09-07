cab3befd548a649d20454ab7d6273a4200f6c800
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2021-09-07 14:45:29 -0300

    Requisito 10

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,8 +1,22 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, nick) {
+  // parametros array de tecnologias e 'name'
+  // ordenar o arrayTechs  e inserir  junto com name as posições em forma de objeto em um novo array
+  let arrayTechs = [];
+  for (let index = 0; index < array.length; index += 1) {
+    let arrayOrdenado = array.sort();
+    let objeto = {
+      tech: arrayOrdenado[index],
+      name: nick,
+    }
+    arrayTechs.push(objeto);
+  }
+  if (arrayTechs.length > 0) {
+    return arrayTechs
+  } else {
+    return 'Vazio!'
+  }
 }
-
 // Desafio 11
 function generatePhoneNumber() {
   // seu código aqui
